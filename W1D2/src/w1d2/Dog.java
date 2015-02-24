3aca879dbf655551f6cd610791fbb800fa24079b
Niloofarm <niloofar.mofrad@gmail.com>
2015-02-24 14:07:44 -0600

    DogTestDrive

---
+++ W1D2/src/w1d2/Dog.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w1d2;
+
+/**
+ *
+ * @author mofrad
+ */
+public class Dog {
+    int size;
+    String breed;
+    String name; 
+    
+    void bark() {
+        System.out.println("Ruff!Ruff!");
+    }
+}
+    
---
+++ W1D2/src/w1d2/DogTestDrive.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w1d2;
+
+/**
+ *
+ * @author mofrad
+ */
+public class DogTestDrive {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+    
+}
