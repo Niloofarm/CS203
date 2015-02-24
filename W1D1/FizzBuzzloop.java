98ce99fec572af86a98c44847605e322a5edfc39
Niloofarm <niloofar.mofrad@gmail.com>
2015-02-24 09:18:45 -0600

    Fizz buzz For loop

---
+++ W1D1/FizzBuzzloop.java
@@ -0,0 +1,15 @@
+public calss FizzBuzzloop {
+	public static void meain ( String[] args ) {
+		for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz")
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+            	System.out.println((int)i);
+			}
+		}
+	}
+}
