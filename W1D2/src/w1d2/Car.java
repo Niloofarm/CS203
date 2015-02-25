bd6d60b872d1ad776e59c369af9b5442301d7f10
Niloofarm <niloofar.mofrad@gmail.com>
2015-02-25 09:12:20 -0600

    Car

---
+++ W1D2/src/w1d2/Car.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w1d2;
+
+/**
+ *
+ * @author mofrad
+ */
+public class Car {
+    private int x = 0;
+    private int y = 0;
+    
+    void moveRight() {
+        x+= 1;
+    }    
+    void moveLeft() {
+        x+= 1;
+    }
+    void moveUp(){
+       y+=1;
+      
+    }
+    void moveDown(){
+       y+=1;
+    }
+    void printLocation(){
+        this.x = x;
+        this.y = y;
+        System.out.print(int x, int y);
+       
+    }
+}
+        
+    
+    
+}
