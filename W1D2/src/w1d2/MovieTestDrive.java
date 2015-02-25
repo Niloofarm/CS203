8a181ffcba91dc521b3be251519caa175090e90d
Niloofarm <niloofar.mofrad@gmail.com>
2015-02-25 09:11:28 -0600

    MovieTestDrive

---
+++ W1D2/src/w1d2/MovieTestDrive.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w1d2;
+
+/**
+ *
+ * @author mofrad
+ */
+public class MovieTestDrive {
+    public static void main (String[] args) {
+     Movie one = new Movie();
+     one.title = "Gone with the Stock";
+     one.genre = "Tragic";
+     one.rating = -2;
+     Movie two = new Movie();
+     two.title = "Lost in cubicle Space";
+     two.genre = "Comedy";
+     two.rating = 5;
+     two.playIt();
+     Movie three = new Movie();
+     three.title = "Byte Club";
+     three.genre = "Tragic but ultimately uplifting";
+     three.rating = 127; 
+    }
+     
+}
