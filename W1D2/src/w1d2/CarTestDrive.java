5d6ed2d62217d71b560143385388d98ceba47041
Niloofarm <niloofar.mofrad@gmail.com>
2015-02-25 09:12:07 -0600

    CarTestDrive

---
+++ W1D2/src/w1d2/CarTestDrive.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w1d2;
+
+/**
+ *
+ * @author mofrad
+ */
+public class CarTestDrive {
+    public static void main(String[] args) {
+        Car mycar = new Car();
+        mycar.moveDown();
+        mycar.moveUp();
+        mycar.moveLeft();
+        mycar.moveRight();
+       
+        mycar.printLocation();
+    
+                
+   
+        
+        Car hercar = new Car();
+        hercar.moveRight();
+        hercar.moveLeft();
+        hercar.moveUp();
+        hercar.moveDown();
+        hercar.printLocation();
+        
+        
+    }
+}
